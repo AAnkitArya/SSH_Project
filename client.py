d92ae160bd411e58680db739b22c6dce146306b7
Ankit Arya <ankit.3arya@gmail.com>
2026-05-23 17:02:42 +0530

    finally done phase 3

--- client.py
+++ client.py
@@ -1,25 +1,50 @@
 import socket 
 import os
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import hashes, serialization
 
 
-KEY = b"keykeykeykeykeykeykeykeykeykeyke"  
+ 
 
 def client():
     c=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print("Connecting")
     c.connect(('127.0.0.1', 8888))
 
-    message = "Chill hai"
-    bytes=message.encode('utf-8')
-    aesgcm = AESGCM(KEY)
+
+
+    server_public_key_bytes = c.recv(1024)
+    server_public_key = serialization.load_pem_public_key(server_public_key_bytes) #bytes to object
+
+    session_key = os.urandom(32) #random 32 bytes
+
+    encrypted_session_key = server_public_key.encrypt(
+        session_key,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+    c.sendall(encrypted_session_key)   #send the key after mixing it with public key received from server
+
+
+
+
+    message = "Chill hai boss"
+
+    aesgcm=AESGCM(session_key)  #khud key key se encrypt ye karega
     nonce = os.urandom(12)
-    ciphertext = aesgcm.encrypt(nonce, bytes, None)
-    payload=nonce+ciphertext
-    c.send(payload)
+    ciphertext = aesgcm.encrypt(nonce, message.encode('utf-8'), None) #aesgcm object already has the session key from the last line. nonce and session key both will be used to encrypt it, nonce for uniquiness so that hacker cannot and see if there are identical packets.
+    c.sendall(nonce+ciphertext)
     print("message sent")
     c.close()
 
+
+
+    
 if __name__ == "__main__":
     client()
 
--- server.py
+++ server.py
@@ -2,11 +2,22 @@
 import socket               
 import os
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
-KEY = b"keykeykeykeykeykeykeykeykeykeyke"
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import hashes, serialization
+
+
+
 
 
 
 def run_server():
+
+    private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+    public_key=private_key.public_key()
+    pem_public_key = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
@@ -17,12 +28,26 @@ def run_server():
     print("listening on port 8888")
     conn,addr=s.accept()
     print(f"connection estb from port:{addr}")
+    conn.sendall(pem_public_key) #sendall use kiya kyonki send se kam data bhej sakte hain
+    print("public key sent")
+    encrypted_session_key = conn.recv(256) #pehle server bheja public key, then client multiplied its own with it and sent it back 
+    session_key = private_key.decrypt(
+        encrypted_session_key,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    print(f"got it (Length: {len(session_key)})")
+
+
     payload = conn.recv(1024)
     if len(payload) > 12:
         nonce = payload[:12]
         ciphertext = payload[12:]
 
-        aesgcm = AESGCM(KEY)
+        aesgcm = AESGCM(session_key)
     
         try:
             decrypted_bytes=aesgcm.decrypt(nonce,ciphertext,None)
