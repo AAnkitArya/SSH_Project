ff6a6701d07b95cfff53cb2937b99860e27d261c
Ankit Arya <ankit.3arya@gmail.com>
2026-05-23 14:28:15 +0530

    PHASE 1 complete, also fixed TIMEWAIT for 2 mins on port 8088 after killing the occupying process

--- server.py
+++ server.py
@@ -6,6 +6,10 @@ import socket
 
 def run_server():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
     s.bind(('127.0.0.1', 8888))
     s.listen(1)
     print("listening on port 8888")
@@ -15,7 +19,7 @@ def run_server():
     plaintext_message = raw_data.decode('utf-8')
     print(f"message: {plaintext_message}")
     conn.close()
-    server_socket.close()
+    s.close()
 
 if __name__ == "__main__":
     run_server()
