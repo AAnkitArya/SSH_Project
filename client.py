ffbd38264bc968d1bddeeede2cc8ddcf49a18d71
Ankit Arya <ankit.3arya@gmail.com>
2026-05-23 14:21:17 +0530

    basic client.py setup

--- client.py
+++ client.py
@@ -1,2 +1,16 @@
 import socket               
-import os
+
+def client():
+    c=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("Connecting")
+    c.connect(('127.0.0.1', 8888))
+
+    message = "Chill hai"
+    c.send(message.encode('utf-8'))
+    print("message sent")
+    c.close()
+
+if __name__ == "__main__":
+    client()
+
+
